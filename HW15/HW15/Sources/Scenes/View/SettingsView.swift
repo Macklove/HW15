17b2d03e13078815205ff5825cec102c2c43d518
Makulov Evgeny <makulovev@gmail.com>
2022-06-15 20:36:52 +0300

    Add table configurated

--- HW15/HW15/Sources/Scenes/View/SettingsView.swift
+++ HW15/HW15/Sources/Scenes/View/SettingsView.swift
@@ -9,26 +9,69 @@ import UIKit
 
 final class SettingsView: UIView {
 
-        // MARK: - Configuration
-        func configureView(with models: [SettingsGroup]) {
-            self.models = models
-            settingsTable.reloadData()
-        }
+    // MARK: - Configuration
+    func configureView(with models: [SettingsGroup]) {
+        self.models = models
+        settingsTable.reloadData()
+    }
 
-        // MARK: - Private properties
-        private var models = [SettingsGroup]()
+    // MARK: - Private properties
 
-        // MARK: - Views
-        private lazy var settingsTable: UITableView = {
-            let table = UITableView(frame: .zero, style: .grouped)
-            table.register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.identifier)
-            table.translatesAutoresizingMaskIntoConstraints = false
-            
-            table.delegate = self
-            table.dataSource = self
+    private var models = [SettingsGroup]()
 
-            return table
-        }()
+    // MARK: - Views
+
+    private lazy var settingsTable: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.identifier)
+        table.translatesAutoresizingMaskIntoConstraints = false
+        
+        table.delegate = self
+        table.dataSource = self
+
+        return table
+    }()
+
+    // MARK: - Initial
+
+    init() {
+        super.init(frame: .zero)
+        commonInit()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        backgroundColor = .white
+        setupHierarchy()
+        setupLayout()
+    }
+
+    // MARK: - Settings
+
+    private func setupHierarchy() {
+        addSubview(settingsTable)
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            settingsTable.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            settingsTable.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            settingsTable.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            settingsTable.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    func setupNavigation(_ sender: UIViewController) {
+        sender.navigationItem.title = "Настройки"
+        sender.navigationController?.navigationBar.backgroundColor = .white
+        let attributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.semibold)]
+        sender.navigationController?.navigationBar.titleTextAttributes = attributes
+    }
+    
 }
 
 // MARK: - UITableViewDataSource
