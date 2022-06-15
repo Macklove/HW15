29df5552a144b251ed55a8dfc5cb6c1edc473241
Makulov Evgeny <makulovev@gmail.com>
2022-06-15 19:35:55 +0300

    Change viewDidLoad

--- HW15/HW15/Sources/Scenes/Controller/SettingsController.swift
+++ HW15/HW15/Sources/Scenes/Controller/SettingsController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class SettingsController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var model: SettingsModel?
+
+    private var settingsView: SettingsView? {
+        guard isViewLoaded else { return nil }
+        return view as? SettingsView
     }
 
+// MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view = SettingsView()
+        model = SettingsModel()
+    }
 
 }
+
 
